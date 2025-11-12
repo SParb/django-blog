f6cbd7db6419a0fee1eef5420e96134b789b4d15
SParb <ryuchigo@gmail.com>
2025-11-12 12:37:33 +0000

    add local host ip to allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#9$1-)thm7%cgow*7jf$+c9n1@hhh@myz+9(g!c27f+pgyacf_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
