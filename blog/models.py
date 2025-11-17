ff3acd789eed658c7f0e2f7bae524815daba6bc1
SParb <ryuchigo@gmail.com>
2025-11-17 15:00:39 +0000

    change database due to bug and add viewable comments to posts

--- blog/models.py
+++ blog/models.py
@@ -23,9 +23,9 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
-    body = models.TextField()
-    approved = models.BooleanField(default=False)
+    body = models.TextField()   
     created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["created_on"]
